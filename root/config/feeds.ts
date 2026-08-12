47478baaec212fbbec9ca0620d71d6912bf94374
J2Alvin <138383819+J2Alvin@users.noreply.github.com>
2026-08-12 19:52:57 +0530

    Create feeds.ts

---
+++ root/config/feeds.ts
@@ -0,0 +1,11 @@
+export const FEEDS: string[] = [
+  "https://news.google.com/rss/search?q=site:reuters.com&hl=en-IN&gl=IN&ceid=IN:en",
+  "https://www.thehindu.com/feeder/default.rss", // Updated to main top news feed
+  "https://www.livemint.com/rss/news",
+  "https://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+  "https://economictimes.indiatimes.com/rssfeedstopstories.cms",
+  "https://feeds.bbci.co.uk/news/rss.xml",
+  "http://rss.cnn.com/rss/edition.rss",
+  "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
+  "https://news.google.com/rss/search?q=site:thewire.in&hl=en-IN&gl=IN&ceid=IN:en",
+];
